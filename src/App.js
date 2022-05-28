e0f2fbcd2d94d9293ac1eab0db6c45b8b65d3c23
Patrícia Cavalcante <patriciacavalcante@gmail.com>
2022-05-28 16:01:21 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Navbar from "./components/Navbar"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold bg-slate-500 p-10 text-center">
-      Hello world!
-    </h1>
+    <>
+    <Navbar />
+    </>
   )
 }
